ff1dde172e15e2f67ec6fb3562d1de383eaa7d80
Jeehyae dance <jeehyae91@gmail.com>
2020-02-20 18:13:25 -0800

    added inner route for changing username

--- client/src/Setting.jsx
+++ client/src/Setting.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route, Link, withRouter } from "react-router-dom";
+import Option from "./Option.jsx";
 
 class Setting extends React.Component {
   constructor(props) {
@@ -7,12 +9,26 @@ class Setting extends React.Component {
   }
 
   render() {
+    let { path, url } = this.props.match;
     return (
       <div>
         <h1>Setting</h1>
+        <div>
+          <span>
+            <Link to={`${url}/changeUserName`}>Changing User Name</Link>
+          </span>
+        </div>
+        <Switch>
+          <Route exact path={path}>
+            <span>Please select option.</span>
+          </Route>
+          <Route path={`${path}/:option`}>
+            <Option />
+          </Route>
+        </Switch>
       </div>
     );
   }
 }
 
-export default Setting;
+export default withRouter(Setting);
