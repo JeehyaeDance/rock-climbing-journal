ee0bec5837cebd3b65b1c33e83552b22ae66802b
Jeehyae dance <jeehyae91@gmail.com>
2020-02-23 01:33:20 -0800

    applied style to splash page

--- client/src/Splash.jsx
+++ client/src/Splash.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import styles from "./style/Splash.css";
 
 function Splash() {
   return (
-    <div>
+    <div className={styles.allFont}>
       <h2>Climbing Day</h2>
-      <div>
-        <span>The best way to track your rock climbing</span>
-        <span>
-          Climbing Day is a rock climbing journal application that is used to track your day to day rock climbing
-          activity and progress. If you are looking to improve in rock climbing, give Climbing Day a try!
-        </span>
-      </div>
-      <div>
-        <button>Create Account</button>
-        <div>
-          <span>Already have an account?</span>
-          <span>Login</span>
+      <div className={styles.centerContents}>
+        <div className={styles.centerExp}>
+          <span className={styles.mainExp}>The best way to track your rock climbing</span>
+          <span className={styles.subExp}>
+            Climbing Day is a rock climbing journal application that is used to track your day to day rock climbing
+            activity and progress. If you are looking to improve in rock climbing, give Climbing Day a try!
+          </span>
+        </div>
+        <div className={styles.formBox}>
+          <span className={styles.button}>Create Account</span>
+          <div className={styles.loginOption}>
+            <span>Already have an account?</span>
+            <span className={styles.login}>Login</span>
+          </div>
         </div>
       </div>
     </div>
