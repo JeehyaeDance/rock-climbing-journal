034eba179a3b220f8f8056094297068a72414dac
Jeehyae dance <jeehyae91@gmail.com>
2020-02-24 00:09:27 -0800

    updated and applied Page component to splash page

--- client/src/Splash.jsx
+++ client/src/Splash.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import styles from "./style/Splash.css";
-import Header from "./Header.jsx";
 import { Link } from "react-router-dom";
+import Page from "./Page";
 
 function Splash() {
   return (
-    <div className={styles.allFont}>
-      <Header />
-      <div className={styles.centerContents}>
-        <div className={styles.centerExp}>
-          <span className={styles.mainExp}>The best way to track your rock climbing</span>
-          <span className={styles.subExp}>
-            Climbing Day is a rock climbing journal application that is used to track your day to day rock climbing
-            activity and progress. If you are looking to improve in rock climbing, give Climbing Day a try!
-          </span>
-        </div>
-        <div className={styles.formBox}>
-          <Link to="/createAcc" className={styles.button}>
-            Create Account
-          </Link>
-          <div className={styles.loginOption}>
-            <span>Already have an account?</span>
-            <Link to="/loginPage" className={styles.login}>
-              Login
-            </Link>
-          </div>
-        </div>
+    <Page>
+      <div className={styles["text-container"]}>
+        <h1 className={styles.title}>The best way to track your rock climbing</h1>
+        <p className={styles.description}>
+          Climbing Day is a rock climbing journal application that is used to track your day to day rock climbing
+          activity and progress. If you are looking to improve in rock climbing, give Climbing Day a try!
+        </p>
       </div>
-    </div>
+      <div className={styles["form-container"]}>
+        <Link to="/createAcc" className={styles.button}>
+          Create Account
+        </Link>
+
+        <span className={styles["login-text"]}>
+          Already have an account?
+          <Link to="/loginPage" className={styles.login}>
+            Login
+          </Link>
+        </span>
+      </div>
+    </Page>
   );
 }
 
