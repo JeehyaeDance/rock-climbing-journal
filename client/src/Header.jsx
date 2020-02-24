4d24dd3f4791b1f2944edc7f0236e17d9775ee64
Jeehyae dance <jeehyae91@gmail.com>
2020-02-23 22:32:20 -0800

    created header component to re-use on other components

---
+++ client/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style/Header.css";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <h2 className={styles.title}>Climbing Day</h2>
+    </div>
+  );
+}
+
+export default Header;
