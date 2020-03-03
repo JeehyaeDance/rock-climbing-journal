cb3e3f55e81f79e666047548f5826553de0f5904
Jeehyae dance <jeehyae91@gmail.com>
2020-03-02 23:41:17 -0800

    applied radio button component to log page

--- client/src/LogPage.jsx
+++ client/src/LogPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./style/LogPage.css";
 import axios from "axios";
+import RadioBtn from "./RadioBtn.jsx";
 
 class LogPage extends React.Component {
   constructor(props) {
@@ -49,51 +50,22 @@ class LogPage extends React.Component {
 
   render() {
     const { level, isLogged, note } = this.state;
+
     return (
       <div className={styles.centerPiece}>
         <form className={styles.formStyle}>
           <label className={styles["form-label"]}>Select Level</label>
           <div className={styles["radio-group"]}>
-            <input type="radio" id="V0" name="level" value={0} checked={level === 0} readOnly />
-            <label htmlFor="V0" onClick={() => this.handleRadioChange(0)}>
-              V0
-            </label>
-            <input type="radio" id="V1" name="level" value={1} checked={level === 1} readOnly />
-            <label htmlFor="V1" onClick={() => this.handleRadioChange(1)}>
-              V1
-            </label>
-            <input type="radio" id="V2" name="level" value={2} checked={level === 2} readOnly />
-            <label htmlFor="V2" onClick={() => this.handleRadioChange(2)}>
-              V2
-            </label>
-            <input type="radio" id="V3" name="level" value={3} checked={level === 3} readOnly />
-            <label htmlFor="V3" onClick={() => this.handleRadioChange(3)}>
-              V3
-            </label>
-            <input type="radio" id="V4" name="level" value={4} checked={level === 4} readOnly />
-            <label htmlFor="V4" onClick={() => this.handleRadioChange(4)}>
-              V4
-            </label>
-            <input type="radio" id="V5" name="level" value={5} checked={level === 5} readOnly />
-            <label htmlFor="V5" onClick={() => this.handleRadioChange(5)}>
-              V5
-            </label>
-            <input type="radio" id="V6" name="level" value={6} checked={level === 6} readOnly />
-            <label htmlFor="V6" onClick={() => this.handleRadioChange(6)}>
-              V6
-            </label>
-            <input type="radio" id="V7" name="level" value={7} checked={level === 7} readOnly />
-            <label htmlFor="V7" onClick={() => this.handleRadioChange(7)}>
-              V7
-            </label>
-            <input type="radio" id="V8" name="level" value={8} checked={level === 8} readOnly />
-            <label htmlFor="V8" onClick={() => this.handleRadioChange(8)}>
-              V8
-            </label>
-            <input type="radio" id="V9" name="level" value={9} checked={level === 9} readOnly />
-            <label htmlFor="V9" onClick={() => this.handleRadioChange(9)}>
-              V9
-            </label>
+            <RadioBtn level={0} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={1} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={2} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={3} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={4} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={5} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={6} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={7} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={8} stateLevel={level} handleRadioChange={this.handleRadioChange} />
+            <RadioBtn level={9} stateLevel={level} handleRadioChange={this.handleRadioChange} />
           </div>
           <label className={styles["form-label"]}>Add Note</label>
           <textarea className={styles.noteBox} id="note" value={note} onChange={this.handleNoteChange}></textarea>
--- client/src/RadioBtn.jsx
+++ client/src/RadioBtn.jsx
@@ -10,10 +10,15 @@ function RadioBtn(props) {
         name="level"
         value={props.level}
         checked={props.stateLevel === props.level}
+        className={styles["radio-input"]}
         readOnly
       />
-      <label htmlFor={props.level} onClick={() => this.handleRadioChange(props.level)}>
-        V0
+      <label
+        htmlFor={props.level}
+        className={styles["radio-label"]}
+        onClick={() => props.handleRadioChange(props.level)}
+      >
+        V{props.level}
       </label>
     </>
   );
