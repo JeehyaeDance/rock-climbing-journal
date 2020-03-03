08a0fb74c20fba0075b3ae5490aa5c3a96f12f51
Jeehyae dance <jeehyae91@gmail.com>
2020-03-02 22:43:40 -0800

    edited style for submit button on log page

--- client/src/LogPage.jsx
+++ client/src/LogPage.jsx
@@ -13,6 +13,7 @@ class LogPage extends React.Component {
     };
     this.handleRadioChange = this.handleRadioChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleNoteChange = this.handleNoteChange.bind(this);
   }
 
   handleRadioChange(value) {
@@ -21,6 +22,11 @@ class LogPage extends React.Component {
     });
   }
 
+  handleNoteChange(e) {
+    this.setState({
+      note: e.target.value
+    });
+  }
   handleClick(e) {
     e.preventDefault();
     axios
@@ -32,7 +38,7 @@ class LogPage extends React.Component {
       .then(response => {
         this.setState({
           isLogged: !this.state.isLogged,
-          level: 0,
+          level: 10,
           note: ""
         });
       })
@@ -46,66 +52,55 @@ class LogPage extends React.Component {
     return (
       <div className={styles.centerPiece}>
         <form className={styles.formStyle}>
-          <label>Select Level</label>
+          <label className={styles["form-label"]}>Select Level</label>
           <div className={styles["radio-group"]}>
-            <input type="radio" id="V0" name="level" value={0} checked={level === 0} />
+            <input type="radio" id="V0" name="level" value={0} checked={level === 0} readOnly />
             <label htmlFor="V0" onClick={() => this.handleRadioChange(0)}>
               V0
             </label>
-            <input type="radio" id="V1" name="level" value={1} checked={level === 1} />
+            <input type="radio" id="V1" name="level" value={1} checked={level === 1} readOnly />
             <label htmlFor="V1" onClick={() => this.handleRadioChange(1)}>
               V1
             </label>
-            <input type="radio" id="V2" name="level" value={2} checked={level === 2} />
+            <input type="radio" id="V2" name="level" value={2} checked={level === 2} readOnly />
             <label htmlFor="V2" onClick={() => this.handleRadioChange(2)}>
               V2
             </label>
-            <input type="radio" id="V3" name="level" value={3} checked={level === 3} />
+            <input type="radio" id="V3" name="level" value={3} checked={level === 3} readOnly />
             <label htmlFor="V3" onClick={() => this.handleRadioChange(3)}>
               V3
             </label>
-            <input type="radio" id="V4" name="level" value={4} checked={level === 4} />
+            <input type="radio" id="V4" name="level" value={4} checked={level === 4} readOnly />
             <label htmlFor="V4" onClick={() => this.handleRadioChange(4)}>
               V4
             </label>
-            <input type="radio" id="V5" name="level" value={5} checked={level === 5} />
+            <input type="radio" id="V5" name="level" value={5} checked={level === 5} readOnly />
             <label htmlFor="V5" onClick={() => this.handleRadioChange(5)}>
               V5
             </label>
-            <input type="radio" id="V6" name="level" value={6} checked={level === 6} />
+            <input type="radio" id="V6" name="level" value={6} checked={level === 6} readOnly />
             <label htmlFor="V6" onClick={() => this.handleRadioChange(6)}>
               V6
             </label>
-            <input type="radio" id="V7" name="level" value={7} checked={level === 7} />
+            <input type="radio" id="V7" name="level" value={7} checked={level === 7} readOnly />
             <label htmlFor="V7" onClick={() => this.handleRadioChange(7)}>
               V7
             </label>
-            <input type="radio" id="V8" name="level" value={8} checked={level === 8} />
+            <input type="radio" id="V8" name="level" value={8} checked={level === 8} readOnly />
             <label htmlFor="V8" onClick={() => this.handleRadioChange(8)}>
               V8
             </label>
-            <input type="radio" id="V9" name="level" value={9} checked={level === 9} />
+            <input type="radio" id="V9" name="level" value={9} checked={level === 9} readOnly />
             <label htmlFor="V9" onClick={() => this.handleRadioChange(9)}>
               V9
             </label>
           </div>
-          {/* <select id="level" value={level} onChange={this.handleChange}>
-            <option value="0">V0</option>
-            <option value="1">V1</option>
-            <option value="2">V2</option>
-            <option value="3">V3</option>
-            <option value="4">V4</option>
-            <option value="5">V5</option>
-            <option value="6">V6</option>
-            <option value="7">V7</option>
-            <option value="8">V8</option>
-            <option value="9">V9</option>
-            <option value="10">V10</option>
-          </select> */}
-          <label>Add Note</label>
-          <textarea className={styles.noteBox} id="note" value={note} onChange={this.handleChange}></textarea>
+          <label className={styles["form-label"]}>Add Note</label>
+          <textarea className={styles.noteBox} id="note" value={note} onChange={this.handleNoteChange}></textarea>
         </form>
-        <button onClick={this.handleClick}>Save</button>
+        <button className={styles.button} onClick={this.handleClick}>
+          Send it!
+        </button>
         {isLogged ? <span className={styles.saveMsg}>Your log is saved! 🧗🏻‍♀️</span> : null}
       </div>
     );
