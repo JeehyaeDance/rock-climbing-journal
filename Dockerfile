b1726094c5b80a7a0961ed9d7e925d17015f87cb
Jeehyae dance <jeehyae91@gmail.com>
2020-03-11 20:11:51 -0700

    updated dockerfile and database connection with upated environtment variable

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN npm install
 
 # What port will the container talk to the outside world with once created?
 EXPOSE 3000
-EXPOSE 3001
+EXPOSE 3001 
 
 # How do you start your app?
 CMD [ "npm", "start" ]
--- database/index.js
+++ database/index.js
@@ -1,12 +1,19 @@
 const { Pool } = require("pg");
 
-console.log(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PORT);
+console.log("environment: ", process.env.NODE_ENV);
 
-let pool = new Pool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+let config =
+  process.env.NODE_ENV === "development"
+    ? { host: process.env.DB_HOST, database: process.env.DB_NAME, port: process.env.DB_PORT }
+    : {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+      };
+
+let pool = new Pool(config);
 
 pool.on("error", err => {
   console.log(err);
